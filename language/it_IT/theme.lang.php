5d08f7422b8c94dc923b727a8bd0e92b962c34e6
riopwg <rio@piwigo.org>
2011-05-10 13:54:10 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/hr_os_xl@10831 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/theme.lang.php
+++ language/it_IT/theme.lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['Display page banner on home page'] = 'Visualizza il banner sulla pagina principale';
-$lang['Display page banner on categories pages'] = 'Visualizza il banner sulla pagina delle categorie';
+$lang['Display page banner on categories pages'] = 'Visualizza il banner sulla pagina degli album';
 $lang['Display page banner on picture page'] = 'Visualizza il banner sulla pagine delle foto';
 $lang['Display page banner on other pages'] = 'Visualizza banner sulle altre pagine';
 

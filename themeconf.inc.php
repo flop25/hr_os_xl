c0edce71fa6bf19a7fd441ef719909ed61a8c686
flop25 <flop25@piwigo.org>
2011-09-29 16:38:24 +0000

    deleting the "spreadmenu" system adding a check for config
    git-svn-id: http://piwigo.org/svn/extensions/hr_os_xl@12266 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -8,20 +8,29 @@ Author: flop25
 Author URI: http://www.planete-flop.fr
 */
 $themeconf = array(
-  'name'					=> 'hr_os_xl',
+  'name'          => 'hr_os_xl',
   'parent'        => 'default',
   'icon_dir'      => 'themes/hr_os_xl/icon',
   'mime_icon_dir' => 'themes/hr_os_xl/icon/mimetypes/',
-  'local_head'		=> 'local_head.tpl',
-  'activable'			=> true,
-	'add_menu_on_public_pages'			=> true,	# activation
-	'Exclude'				=> array('theNBMPage','thePopuphelpPage',),	# Excluded pages
+  'local_head'    => 'local_head.tpl',
+  'activable'     => true,
 );
-@include(PHPWG_ROOT_PATH. 'local/config/hr_os_xl.inc.php');
-if (isset($conf['local_dir_site']))
-{
-  @include(PHPWG_ROOT_PATH.PWG_LOCAL_DIR. 'config/hr_os_xl.inc.php');
-}
+  if (!isset($conf['hr_os_xl']))
+  {
+    $config = array(
+      'home'       => true,
+      'categories' => true,
+      'picture'    => false,
+      'other'      => true,
+      );
+      
+    $query = '
+INSERT INTO ' . CONFIG_TABLE . ' (param,value,comment)
+VALUES ("hr_os_xl" , "'.pwg_db_real_escape_string(serialize($config)).'" , "hr_os_xl parameters");';
+
+    pwg_query($query);
+    load_conf_from_db();
+  }
 
 // thx to P@t
 add_event_handler('loc_begin_page_header', 'set_hr_os_xl_header');
@@ -46,33 +55,5 @@ function set_hr_os_xl_header()
   }
 
   $template->assign('display_hr_os_xl_banner', $header);
-}
-// thx to Vdigital and his plugin spreadmenus
-if ( !function_exists( 'add_menu_on_public_pages' ) ) {
-	if ( defined('IN_ADMIN') and IN_ADMIN ) return false;
-	add_event_handler('loc_after_page_header', 'add_menu_on_public_pages', 20);
-
-	function  add_menu_on_public_pages() {
-	  if ( function_exists( 'initialize_menu') ) return false; # The current page has already the menu 
-	  if ( !get_themeconf('add_menu_on_public_pages') ) return false; # The current page has already the menu 
-	  global $template, $page, $conf;
-	  if ( isset($page['body_id']) and in_array($page['body_id'], get_themeconf('Exclude')) ) return false;
-
-	  $template->set_filenames(array(
-		'add_menu_on_public_pages' => dirname(__FILE__) . '/template/add_menu_on_public_pages.tpl',
-	  ));
-	  include_once(PHPWG_ROOT_PATH.'include/menubar.inc.php');
-	  $template->parse('add_menu_on_public_pages');
-	  
-	  if (is_admin())
-		{
-	  $template->assign(
-        'U_ADMIN', get_root_url().'admin.php?page=picture_modify'
-      .'&amp;cat_id='.(isset($page['category']) ? $page['category']['id'] : '')
-      .( isset($page['image_id']) ? '&amp;image_id='.$page['image_id'] : '')
-      );
-		}
-	  
-	}
 }
 ?>

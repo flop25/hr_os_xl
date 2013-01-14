f825206cc9ce7ced7da243474422e90f9a1a7869
flop25 <flop25@piwigo.org>
2013-01-14 19:17:38 +0000

    language files propagation
    git-svn-id: http://piwigo.org/svn/extensions/hr_os_xl@20155 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/theme.lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['Display page banner on home page'] = 'Εμφάνιση του banner στην αρχική σελίδα';
+$lang['Display page banner on categories pages'] = 'Εμφάνιση του banner στη σελίδα κατηγοριών';
+$lang['Display page banner on picture page'] = 'Εμφάνιση του banner στη σελίδα φωτογραφίας';
+$lang['Display page banner on other pages'] = 'Εμφάνιση του banner στις άλλες σελίδες'; 
+
+?>
